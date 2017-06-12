31e7f3367eb57094688d4f4439b3944282588e5c
Kristina Martinez <misskristinamartinez@gmail.com>
2017-06-12 06:49:55 -0400

    added components and React Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
-import Body from './Body';
+import './index.css';
+import Members from './Members';
 import Footer from './Footer';
 import Header from './Header';
+import Home from './Home';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
 
 class App extends Component {
 
   
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Body />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />        
+          <div className="middle">
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/Members">Members</Link></li>
+            </ul>
+
+            <hr/>
+
+            <Route exact path="/" component={Home}/>
+            <Route path="/Members" component={Members}/>
+          </div>        
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
--- src/Body.js
+++ src/Body.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
+import Members from './Members';
+
 class Body extends Component {
 
   render() {
     return (
-      <div className="middle">Middle</div>
+      <div className="middle">
+          <Members />
+          <button className= "button-style"  type="button">Search for Books</button>
+      </div>
     );
   }
 }
