d68648146488dd80bdb4af5eb6264a64d3bb0352
Kristina Martinez <misskristinamartinez@gmail.com>
2017-07-24 07:20:43 -0400

    created About page

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => (
+  <div className="about">
+    <h1>About this app</h1>
+    <h2>This app was created using:</h2>
+    <ul>
+        <li>create-react-app</li>
+        <li>html, css, javascript</li>
+        <li></li>
+    </ul>
+  </div>
+);
+
+export default About;
