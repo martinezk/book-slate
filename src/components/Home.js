6fec75c099045b7c4ebbe7b03a78849f7900792a
Kristina Martinez <misskristinamartinez@gmail.com>
2017-09-05 19:24:19 -0400

    Home.js styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,12 +19,12 @@ class Home extends Component {
           </ul>
         </div>
         <div className="main">    
+          <img src={ require('../images/home2.jpg') }  alt="readers on a train"/>
           <a className="photo-credit" href="http://unsplash.com/@ratz03?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer"
             title="Download free do whatever you want high-resolution photos from Rathish Gandhi">
             <span className="credit-style"><i className="fa fa-camera" aria-hidden="true"></i>   Rathish Gandhi</span>
           </a>
         </div>
-        <h1>Stay connected to friends. Manage your bookclub in one simple place</h1>
       </div>
     );
   }
